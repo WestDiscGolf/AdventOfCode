7c23784378d0286b000e7474a4fecc922e4fd544
Adam Storr <WestDiscGolf@users.noreply.github.com>
2022-12-05 21:08:21 +0000

    Add Day 6 prep

---
+++ 2022/AdventOfCode.Tests/06/Day6.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace AdventOfCode.Tests._06;
+
+public class Day6
+{
+    public string Execute(string filename)
+    {
+        var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\06\\{filename}");
+
+        return "";
+    }
+
+    public string Execute2(string filename)
+    {
+        var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\06\\{filename}");
+
+        return "";
+    }
+
+    [Theory]
+    [InlineData("Example.txt", "")]
+    [InlineData("Input01.txt", "")]
+    public void Run(string file, string answer)
+    {
+        // Arrange
+
+        // Act
+        var result = Execute(file);
+
+        // Assert
+        result.Should().Be(answer);
+    }
+
+    [Theory]
+    [InlineData("Example.txt", "")]
+    [InlineData("Input01.txt", "")]
+    public void Run2(string file, string answer)
+    {
+        // Arrange
+
+        // Act
+        var result = Execute2(file);
+
+        // Assert
+        result.Should().Be(answer);
+    }
+}
