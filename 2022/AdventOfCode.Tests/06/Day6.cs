84b06df278402cca97bbe0aebe3d2ae20408880e
Adam Storr <WestDiscGolf@users.noreply.github.com>
2022-12-06 08:48:34 +0000

    Day 6 Part 1 - Done

--- 2022/AdventOfCode.Tests/06/Day6.cs
+++ 2022/AdventOfCode.Tests/06/Day6.cs
@@ -9,11 +9,49 @@ namespace AdventOfCode.Tests._06;
 
 public class Day6
 {
-    public string Execute(string filename)
+    public int Execute(string input)
     {
-        var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\06\\{filename}");
+        if (input.EndsWith("txt"))
+        {
+            input = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\06\\{input}");
+        }
 
-        return "";
+        int count = 0;
+
+        Queue<char> q = new();
+
+        foreach ((char c, int index) in input.Select((x, i) => (x, i + 1))) // work with 1 based!
+        {
+            if (index <= 4)
+            {
+                q.Enqueue(c);
+            }
+            else
+            {
+                // add the next one
+                q.Enqueue(c);
+
+                // get rid of the last one
+                q.Dequeue();
+
+                // any duplicates?
+                if (!AnyDuplicates(q))
+                {
+                    count = index;
+                    break;
+                }
+            }
+        }
+        
+        return count;
+    }
+
+    public bool AnyDuplicates(Queue<char> q)
+    {
+        char[] copy = new char[4];
+        q.CopyTo(copy, 0);
+        var hash = new HashSet<char>(copy);
+        return hash.Count < 4;
     }
 
     public string Execute2(string filename)
@@ -24,14 +62,17 @@ public class Day6
     }
 
     [Theory]
-    [InlineData("Example.txt", "")]
-    [InlineData("Input01.txt", "")]
-    public void Run(string file, string answer)
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    [InlineData("Input01.txt", 1655)]
+    public void Run(string input, int answer)
     {
         // Arrange
 
         // Act
-        var result = Execute(file);
+        var result = Execute(input);
 
         // Assert
         result.Should().Be(answer);
