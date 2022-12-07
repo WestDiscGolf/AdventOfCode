c5d605611cfe96cb159927e6808eff4255f42009
Adam Storr <WestDiscGolf@users.noreply.github.com>
2022-12-07 23:02:41 +0000

    Add Day 8 prep

---
+++ 2022/AdventOfCode.Tests/08/Day8.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+
+namespace AdventOfCode.Tests._08;
+
+public class Day8
+{
+    public string Execute(string filename)
+    {
+        var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\08\\{filename}");
+
+        return "";
+    }
+
+    public string Execute2(string filename)
+    {
+        var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\08\\{filename}");
+
+        return "";
+    }
+
+    [Theory]
+    [InlineData("Example.txt", "")]
+    [InlineData("Input01.txt", "")]
+    public void Run(string file, string answer)
+    {
+        // Arrange
+
+        // Act
+        var result = Execute(file);
+
+        // Assert
+        result.Should().Be(answer);
+    }
+
+    [Theory]
+    [InlineData("Example.txt", "")]
+    [InlineData("Input01.txt", "")]
+    public void Run2(string file, string answer)
+    {
+        // Arrange
+
+        // Act
+        var result = Execute2(file);
+
+        // Assert
+        result.Should().Be(answer);
+    }
+}
