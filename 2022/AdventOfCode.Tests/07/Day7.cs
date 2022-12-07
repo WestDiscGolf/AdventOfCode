e141473a7023aa8996cb2634bda422cbb0d55478
Adam Storr <WestDiscGolf@users.noreply.github.com>
2022-12-07 13:26:55 +0000

    Add Day 7 prep

--- 2022/AdventOfCode.Tests/07/Day7.cs
+++ 2022/AdventOfCode.Tests/07/Day7.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,4 +9,45 @@ namespace AdventOfCode.Tests._07;
 
 public  class Day7
 {
+    public string Execute(string filename)
+    {
+        var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\07\\{filename}");
+
+        return "";
+    }
+
+    public string Execute2(string filename)
+    {
+        var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\07\\{filename}");
+
+        return "";
+    }
+
+    [Theory]
+    [InlineData("Example.txt", "")]
+    [InlineData("Input01.txt", "")]
+    public void Run(string file, string answer)
+    {
+        // Arrange
+
+        // Act
+        var result = Execute(file);
+
+        // Assert
+        result.Should().Be(answer);
+    }
+
+    [Theory]
+    [InlineData("Example.txt", "")]
+    [InlineData("Input01.txt", "")]
+    public void Run2(string file, string answer)
+    {
+        // Arrange
+
+        // Act
+        var result = Execute2(file);
+
+        // Assert
+        result.Should().Be(answer);
+    }
 }
