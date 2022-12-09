e62388da56ef8c7f90db7ca0c4abe506bea3058d
Adam Storr <WestDiscGolf@users.noreply.github.com>
2022-12-09 18:05:35 +0000

    Day 9 prep

---
+++ 2022/AdventOfCode.Tests/09/Day9.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests._09;
+public class Day9
+{
+    public int Execute(string filename)
+    {
+        var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\09\\{filename}");
+
+        return 0;
+    }
+
+    [Theory]
+    [InlineData("Example.txt", 13)]
+    //[InlineData("Input01.txt", "")]
+    public void Run(string file, int answer)
+    {
+        // Arrange
+
+        // Act
+        var result = Execute(file);
+
+        // Assert
+        result.Should().Be(answer);
+    }
+
+    public string Execute2(string filename)
+    {
+        var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\06\\{filename}");
+
+        return "";
+    }
+
+    [Theory]
+    [InlineData("Example.txt", "")]
+    [InlineData("Input01.txt", "")]
+    public void Run2(string file, string answer)
+    {
+        // Arrange
+
+        // Act
+        var result = Execute2(file);
+
+        // Assert
+        result.Should().Be(answer);
+    }
+}
