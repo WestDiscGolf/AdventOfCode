2040fddeb61d8cc8fde1e6a9799880880df6f799
Adam Storr <WestDiscGolf@users.noreply.github.com>
2022-12-10 18:24:50 +0000

    day 10 wip

--- 2022/AdventOfCode.Tests/10/Day10.cs
+++ 2022/AdventOfCode.Tests/10/Day10.cs
@@ -2,13 +2,32 @@
 
 namespace AdventOfCode.Tests._10;
 
+public record Instruction(int Cycles, int Action);
+
 public class Day10
 {
     public int Execute(string filename)
     {
         var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\10\\{filename}");
+        var lines = raw.SplitOnNewLine();
 
-       
+        var instructions = lines.Select(x =>
+        {
+            return x.SplitBySpace()[0] switch
+            {
+                "addx" => new Instruction(2, int.Parse(x.SplitBySpace()[1])),
+                _ => new Instruction(1, 0)
+            };
+        });
+
+        // number of cycles
+        var cycles = instructions.Select(x => x.Action).Sum();
+
+        // starting value
+        var result = 1;
+
+
+
 
         return 0;
     }
