7a468da4b585cac8c9f54bbace721c3ffbdbc573
Adam Storr <WestDiscGolf@users.noreply.github.com>
2022-12-10 22:42:32 +0000

    Day 10 Part 2 - WIP (aka doesn't work)

--- 2022/AdventOfCode.Tests/10/Day10.cs
+++ 2022/AdventOfCode.Tests/10/Day10.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Text;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.Common.ExtensionFramework;
 
 namespace AdventOfCode.Tests._10;
 
@@ -74,7 +76,89 @@ public class Day10
     {
         var raw = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\10\\{filename}");
 
-       
+        #region CalculateSteps
+        var lines = raw.SplitOnNewLine();
+
+        var instructions = lines.Select(x =>
+        {
+            return x.SplitBySpace()[0] switch
+            {
+                "addx" => new Instruction(Command.Addx, int.Parse(x.SplitBySpace()[1])),
+                _ => new Instruction(Command.Noop, 0)
+            };
+        });
+
+        Dictionary<int, int> steps = new();
+        int currentStep = 1;
+
+        int registerX = 1;
+
+        steps[currentStep] = registerX;
+
+        foreach (var cmd in instructions)
+        {
+            switch (cmd.Cmd)
+            {
+                case Command.Noop:
+                    steps[currentStep + 1] = registerX;
+                    currentStep++;
+                    break;
+
+                case Command.Addx:
+
+                    // keep the current register value
+                    var temp = registerX;
+
+                    // calculate the new value
+                    registerX += cmd.Action;
+
+                    steps[currentStep + 1] = temp;
+                    steps[currentStep + 2] = registerX;
+
+                    currentStep += 2;
+                    break;
+            }
+        }
+        #endregion
+
+        // now we've calculated the steps
+
+
+        // calculate all the points
+
+        // is the sprite visible when the cpu cycles.
+        // if the pixel (or +-1) is on the cpu cycle it will be drawn
+
+        List<string> crt = new();
+
+        for (int cpuCycle = 1; cpuCycle < steps.Count; cpuCycle++)
+        {
+            // if register value is within +- 1 of current then '#'
+            var registerValue = steps.TryGetValue(cpuCycle, out int v) ? v : 1;
+
+            if (Math.Abs(registerValue - cpuCycle) < 2)
+            {
+                crt.Add("#");
+            }
+            else
+            {
+                crt.Add(".");
+            }
+        }
+
+        var chunks = crt.Chunk(40);
+
+        var sb = new StringBuilder();
+        foreach (var chunk in chunks)
+        {
+            sb.AppendLine(string.Join("", chunk));
+        }
+
+        var s = sb.ToString();
+
+        // 0 - 39 etc
+        // chunk it up into groups of 40 .Chunk(40)
+
         return 0;
     }
 
@@ -93,17 +177,17 @@ public class Day10
         result.Should().Be(answer);
     }
 
-    //[Theory(Skip = "Part 2 is WIP")]
+    //[Theory]
     //[InlineData("Example.txt", 8)]
-    ////[InlineData("Input01.txt", "")]
-    //public void Run2(string file, int answer)
-    //{
-    //    // Arrange
+    //[InlineData("Input01.txt", "")]
+    public void Run2(string file, int answer)
+    {
+        // Arrange
 
-    //    // Act
-    //    var result = Execute2(file);
+        // Act
+        var result = Execute2(file);
 
-    //    // Assert
-    //    result.Should().Be(answer);
-    //}
+        // Assert
+        result.Should().Be(answer);
+    }
 }
